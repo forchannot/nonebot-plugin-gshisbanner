1bd919c8591a515ff05e53f153f295e825133530
forchannot <yy320206@gmail.com>
2023-06-01 16:39:40 +0800

    :art: update pluginmetadata

--- nonebot_plugin_gshisbanner/__init__.py
+++ nonebot_plugin_gshisbanner/__init__.py
@@ -7,6 +7,9 @@ __plugin_meta__ = PluginMetadata(
     name="gshisbanner",
     description="这是一个在机器人上获取原神历史卡池的插件",
     usage="XX历史卡池,XX版本卡池，详细查看本仓库readme",
+    type="application",
+    homepage="https://github.com/forchannot/nonebot-plugin-gshisbanner",
+    supported_adapters={"~onebot.v11"},
     extra={
         "author": "forchannot",
         "version": __version__,
